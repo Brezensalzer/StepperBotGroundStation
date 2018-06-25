ec8bab8960fad5787195ce3e6a497b96a3df96bb
Brezensalzer <stefan.labich@googlemail.com>
2018-06-25 14:10:30 +0200

    switched from amqp connection to coap

--- nanoLidar.py
+++ nanoLidar.py
@@ -2,44 +2,10 @@
 # Class for the self made nanoLidar
 #
 
-import pika
-import uuid
+import logging
+import asyncio
+from aiocoap import *
 import math
-
-#------------------------------------
-# setup message queueing
-#------------------------------------
-class lidarRpcClient(object):
-    def __init__(self, hostname,user,passwd):
-        self.cred = pika.PlainCredentials(user, passwd)
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host=hostname,credentials=self.cred))
-
-        self.channel = self.connection.channel()
-
-        result = self.channel.queue_declare(exclusive=True)
-        self.callback_queue = result.method.queue
-
-        self.channel.basic_consume(self.on_response, no_ack=True,
-                                   queue=self.callback_queue)
-
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
-    def call(self, n):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',
-                                   routing_key='lidar_queue',
-                                   properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
-                                         ),
-                                   body=n.encode('utf-8'))
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
 
 #------------------------------------
 # LIDAR class
@@ -47,7 +13,9 @@ class lidarRpcClient(object):
 class nanoLidar:
     'Class for the self made nanoLidar'
     
-    def __init__(self):
+    def __init__(self,host):
+        # coap target
+        self.host = host
         # 360 elements (distance in mm), indexed by angle
         self.map = []
         # scan number
@@ -56,21 +24,38 @@ class nanoLidar:
         self.offset = 17
         # angle correction?
         self.squint = 7.5
-        # initialize message queueing
-        self.lidarRpc = lidarRpcClient('localhost','x200','bone')
         # BreezSLAM compatibility
         self.lidar_data = [()]*360 # 360 elements (distance,quality), indexed by angle
         self.speed_rpm = 0
 
-    def getInfo(self):
-        # read firmware information
-        return self.lidarRpc.call('info#').decode('utf-8')
+    async def __coapGet(self,coap_uri):
+        # initialize coap
+        protocol = await Context.create_client_context()
+        request = Message(code=GET, uri=coap_uri)
+        try: 
+            response = await protocol.request(request).response
+        except Exception as e: 
+            print('Failed to fetch resource:') 
+            print(e)
+        else:
+            # print('Result: %s\n%r'%(response.code, response.payload))
+            return response.payload.decode('utf-8')
 
-    def getScan(self):
+    async def listMethods(self):
+        # list available coap methods
+        response = await self.__coapGet('coap://'+self.host+'/.well-known/core')
+        return response
+
+    async def getInfo(self):
+        # read firmware information
+        response = await self.__coapGet('coap://'+self.host+'/lidar/info')
+        return response
+
+    async def getScan(self):
         # BreezySLAM
         # Returns 360 (distance, quality) tuples.
         # start 360 degree scan
-        lines = self.lidarRpc.call('scan#').decode('utf-8').split('\r\n')
+        lines = await self.__coapGet('coap://'+self.host+'/lidar/scan').split('\r\n')
         # read scan data
         for line in lines:
             if line != '':
